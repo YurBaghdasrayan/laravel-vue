a1992a82022454bd838b95aad6c1c7fdb2dd5ac8
YurBaghdasrayan <yuro.bagdasaryan@bk.ru>
2023-12-11 21:29:22 +0400

    11.12

--- src/pages/HomeComponent.vue
+++ src/pages/HomeComponent.vue
@@ -1,13 +1,17 @@
 <script>
 import axios from "axios";
 import Swal from "sweetalert2";
+// import router from "@/router";
 
 export default {
   // name: "HomeComponent",
   err: {},
   data() {
     return {
-      postcontent:[],
+      form: {
+        search: '',
+      },
+      postcontent: [],
       roleId: null,
       Token: null,
       data: []
@@ -81,6 +85,38 @@ export default {
           });
         }
       });
+    },
+    submitForm() {
+
+      let Token = localStorage.getItem('myObject')
+      let token = JSON.parse(Token).token;
+
+      axios.post('http://127.0.0.1:8000/api/search', {
+        search: this.form.search,
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`,
+        },
+      }).then((res) => {
+        console.log(res)
+      }).catch(error => {
+        console.error('Error fetching data:', error);
+      });
+
+
+      // axios.post('http://127.0.0.1:8000/api/search', {
+      //   search: this.form.search,
+      //   headers: {
+      //     'Content-Type': 'application/json',
+      //     'Authorization': `Bearer ${token}`,
+      //   },
+      // }).then(res => {
+      //   console.log(res)
+      //
+      // }).catch(e => {
+      //   console.error('Error posting data:', e);
+      // })
     }
   }
 }
@@ -88,13 +124,34 @@ export default {
 
 <template>
   <div>
-
   </div>
+  <form>
+    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+    <div class="relative">
+      <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+             fill="none" viewBox="0 0 20 20">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+        </svg>
+      </div>
+      <input               v-model="form.search"
+                           name="search" type="search" id="default-search"
+             class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+             placeholder="Search Mockups, Logos..." required>
+      <button @click="submitForm" type="button"
+              class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        Search
+      </button>
+    </div>
+  </form>
+
   <div class="flex flex-col">
     <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
       <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
         <div class="overflow-hidden">
           <table class="min-w-full text-left text-sm font-light">
+
             <thead class="border-b font-medium dark:border-neutral-500">
             <tr>
               <th scope="col" class="px-6 py-4">Post Id</th>
--- src/pages/admin/UserComponent.vue
+++ src/pages/admin/UserComponent.vue
@@ -4,6 +4,9 @@ import axios from "axios";
 export default {
   data() {
     return {
+      form: {
+        search: ''
+      },
       data: []
     }
   },
@@ -51,12 +54,72 @@ export default {
             console.error('Error fetching data:', error);
           });
     },
+    submitForm() {
+
+      let Token = localStorage.getItem('myObject')
+      let token = JSON.parse(Token).token;
+      const requestData = {
+        search: this.form.search,
+      };
+
+      axios.get('http://127.0.0.1:8000/api/users', {
+        params: requestData,
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`,
+        },
+
+      }, {
+
+      })
+          .then(({data}) => {
+            this.data = data;
+          }).catch(error => {
+        console.error('Error fetching data:', error);
+      });
+
+
+      // axios.post('http://127.0.0.1:8000/api/search', {
+      //   search: this.form.search,
+      //   headers: {
+      //     'Content-Type': 'application/json',
+      //     'Authorization': `Bearer ${token}`,
+      //   },
+      // }).then(res => {
+      //   console.log(res)
+      //
+      // }).catch(e => {
+      //   console.error('Error posting data:', e);
+      // })
+    }
+
   }
 }
 
 </script>
 
 <template>
+  <form>
+    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+    <div class="relative">
+      <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+             fill="none" viewBox="0 0 20 20">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+        </svg>
+      </div>
+      <input v-model="form.search"
+             name="search" type="search" id="default-search"
+             class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+             placeholder="Search Mockups, Logos..." required>
+      <button @click="submitForm" type="button"
+              class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        Search
+      </button>
+    </div>
+  </form>
+
   <div>
     <div class="flex flex-col">
       <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
@@ -72,71 +135,10 @@ export default {
               </tr>
               </thead>
               <tbody v-for="(user, key) in data" :key="key" :asd="asd">
-              <!--              <pre>{{user.id}}</pre>-->
               <tr class="border-b dark:border-neutral-500">
                 <td class="whitespace-nowrap px-6 py-4 font-medium">{{ user.id }}</td>
                 <td class="whitespace-nowrap px-6 py-4">{{ user.name }}</td>
                 <td class="whitespace-nowrap px-6 py-4">{{ user.email }}</td>
-                <!--                <router-link v-if="this.roleId === 3" style="margin-right: 15px;color: black;" :to="{name: 'moderator.show.post', params: {id: asd.id}}">-->
-                <!--                  comment-->
-                <!--                  <td class="whitespace-nowrap px-6 py-4">-->
-                <!--                    <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"-->
-                <!--                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 16">-->
-                <!--                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"-->
-                <!--                            d="M4 8h11m0 0-4-4m4 4-4 4m-5 3H3a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h3"></path>-->
-                <!--                    </svg>-->
-                <!--                  </td>-->
-                <!--                </router-link>-->
-                <!--                <router-link v-if="this.roleId === 2" style="margin-right: 15px;color: black;" :to="{name: 'admin.show.post', params: {id: asd.id}}">-->
-                <!--                  comment-->
-                <!--                  <td class="whitespace-nowrap px-6 py-4">-->
-                <!--                    <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"-->
-                <!--                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 16">-->
-                <!--                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"-->
-                <!--                            d="M4 8h11m0 0-4-4m4 4-4 4m-5 3H3a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h3"></path>-->
-                <!--                    </svg>-->
-                <!--                  </td>-->
-                <!--                </router-link>-->
-
-                <!--                <router-link v-if="this.roleId === 1" style="margin-right: 15px;color: black;" :to="{name: 'show.post', params: {id: asd.id}}">-->
-                <!--                  comment-->
-                <!--                  <td class="whitespace-nowrap px-6 py-4">-->
-                <!--                    <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"-->
-                <!--                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 16">-->
-                <!--                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"-->
-                <!--                            d="M4 8h11m0 0-4-4m4 4-4 4m-5 3H3a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h3"></path>-->
-                <!--                    </svg>-->
-                <!--                  </td>-->
-                <!--                </router-link>-->
-                <!--                <router-link v-if="this.roleId === 1" style="margin-right: 15px;color: black;" :to="{name: 'edit.post', params: {id: asd.id}}">-->
-                <!--                  <button style="border: none;color: blue">-->
-                <!--                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen"-->
-                <!--                         viewBox="0 0 16 16">-->
-                <!--                      <path-->
-                <!--                          d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001m-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>-->
-                <!--                    </svg>-->
-                <!--                  </button>-->
-                <!--                </router-link>-->
-
-                <!--                <router-link v-if="this.roleId === 3" style="margin-right: 15px;color: black;" :to="{name: 'moderator.edit.post', params: {id: asd.id}}">-->
-                <!--                  <button style="border: none;color: blue">-->
-                <!--                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen"-->
-                <!--                         viewBox="0 0 16 16">-->
-                <!--                      <path-->
-                <!--                          d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001m-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>-->
-                <!--                    </svg>-->
-                <!--                  </button>-->
-                <!--                </router-link>-->
-                <!--                <router-link v-if="this.roleId === 2" style="margin-right: 15px;color: black;" :to="{name: 'admin.edit.post', params: {id: asd.id}}">-->
-                <!--                  <button style="border: none;color: blue">-->
-                <!--                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen"-->
-                <!--                         viewBox="0 0 16 16">-->
-                <!--                      <path-->
-                <!--                          d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001m-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>-->
-                <!--                    </svg>-->
-                <!--                  </button>-->
-                <!--                </router-link>-->
-
                 <button type="button" @click="blockUser(user.id)" style="color: red;border: none; margin-right: 15px">
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                        class="bi bi-trash" viewBox="0 0 16 16">
